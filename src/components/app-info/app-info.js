dfc4e83517e9a27c364c9028083c18a6cec47a73
Kirill <kirill1995-1@mail.ru>
2023-08-21 20:02:51 +0300

    CRUD OPERATION

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,16 @@
+import'./app-info.css';
+
+const AppInfo = (props) => {
+    const {increased, employees} = props;
+    
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании Ks.G.</h1>
+            <h2>Общее число сотрудников: {employees}</h2>
+            <h2>Премию получат: {increased}</h2>
+        </div>    
+    );
+
+}
+
+export default AppInfo;
---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,29 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+import './employees-list.css';
+
+const EmployeesList = ({data, onDelete, onToggleProp, changeSalary}) => {
+    
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+        return ( 
+            <EmployeesListItem 
+                key={id}
+                {...itemProps}
+                id={id}
+                onDelete={() => onDelete(id)}
+                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
+                changeSalary={changeSalary}
+                 />
+        )
+        
+    });
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    );
+
+}
+
+export default EmployeesList;
